3c49d1973fbcf5b20761fa059c63f6a7b5658b6f
imrochamatheus <im.rochamatheus@gmail.com>
2021-04-11 18:28:15 -0300

    Implementando áreas de acesso restrito

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,32 +7,34 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1',
-            'status' => 'N',
-            'cnpj' => '00.000.000/000-0',
-            'ddd'=> '11', //São Paulo (SP)
-            'telefone' => '0000-0000'
 
-        ],
-            1 => ['nome' => 'Fornecedor 2',
-            'status' => 'S',
-            'cnpj' => null,
-            'ddd'=> '85', // Fortaleza (CE)
-            'telefone' => '0000-0000'
-        ],
+        /*
+            $fornecedores = [
+                0 => ['nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00.000.000/000-0',
+                'ddd'=> '11', //São Paulo (SP)
+                'telefone' => '0000-0000'
 
-            2 => ['nome' => 'fornecedor 3',
-            'status' => 'S',
-            'cnpj' => null,
-            'ddd'=> '32', // Juiz de Fora (MG)
-            'telefone' => '0000-0000'   
-            
-            ]
-        ];
+            ],
+                1 => ['nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd'=> '85', // Fortaleza (CE)
+                'telefone' => '0000-0000'
+            ],
 
-        
-      
+                2 => ['nome' => 'fornecedor 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd'=> '32', // Juiz de Fora (MG)
+                'telefone' => '0000-0000'   
+                
+                ]
+            ];
         return view('app.fornecedor.index', compact('fornecedores'));
+        */
+
+        return view('app.fornecedor');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,11 @@ Route::post('/login', 'LoginController@autenticar')->name('site.login');
 
 //Agrupamento de rotas                                      Nomeando rotas
 Route::prefix('app')->middleware('autenticacao:padrao, visitante, p3, p4')->group(function(){
-    Route::get('/clientes',function(){return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores','FornecedorController@index')->name('app.fornecedores');
-    Route::get('/produtos',function(){return 'Produtos';})->name('app.produtos');
+    Route::get('/home','HomeController@index')->name('app.home');
+    Route::get('/sair','LoginController@sair')->name('app.sair');
+    Route::get('/cliente','ClienteController@index')->name('app.cliente');
+    Route::get('/fornecedore','FornecedorController@index')->name('app.fornecedor');
+    Route::get('/produto','ProdutoController@index')->name('app.produto');
 });
 
 //Route::get('/teste/{p1}/{p2}','TesteController@teste')->name('teste');
